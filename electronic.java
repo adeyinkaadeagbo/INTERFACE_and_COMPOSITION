a77ab8a6a431cf452e019af27a9127bca7bbd62f
adeyinkaadeagbo <130254148+adeyinkaadeagbo@users.noreply.github.com>
2024-06-09 08:21:02 -0700

    Add files via upload

---
+++ electronic.java
@@ -0,0 +1,19 @@
+public class Main {
+    public interface Electronic {
+
+        boolean onoroff = false;
+    
+        boolean on();
+    
+        boolean off();
+    
+    
+        default  void electronic(){
+            if (onoroff){
+                System.out.println("Default Status is off");
+            }
+        }
+    
+    }
+    
+}
